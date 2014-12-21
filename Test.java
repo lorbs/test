d58a175792fa55f258b0b982a35522ac9c1e1737
Eugene E. Kwan <ekwan16@gmail.com>
2014-12-20 20:12:30 -0500

    added loop

--- Test.java
+++ Test.java
@@ -3,5 +3,7 @@ public class Test
     public static void main(String[] args)
     {
         System.out.println("Hello Universe");
+        for (int i=0; i < 5; i++)
+            System.out.println(i);
     }
 }
